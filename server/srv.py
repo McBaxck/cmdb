895a3153a960fe0e0ba1a64bcbe8aa4197f6aae2
unknown <longuebray@et.esiea.fr>
2022-10-03 17:50:13 +0200

    ajout des routes dans srv.py

--- server/srv.py
+++ server/srv.py
@@ -36,10 +36,63 @@ def create_server():
     return 'put /create/server/'
 
 
+@app.route(f'{BASE_URI}/delete/server', methods=['PUT'])
+def del_server():
+    return 'put /delete/server/'
+
+
 @app.route(f'{BASE_URI}/create/disk/<int:id>', methods=['PUT'])
 def create_disk(id):
     return 'put /create/disk'
 
+@app.route(f'{BASE_URI}/delete/disk/<int:id>', methods=['PUT'])
+def delete_disk(id):
+    return 'put /delete/disk'
+
+@app.route(f'{BASE_URI}/set_hostname/<server>/<hostname>', methods=['PUT'])
+def set_hostname(server, hostname):
+    return 'put /set_hostname/'
+
+@app.route(f'{BASE_URI}/set_partition/<server>/<partition_name>', methods=['PUT'])
+def set_partition(server, partition_name):
+    return 'put /set_partition/'
+
+@app.route(f'{BASE_URI}/delete_partition/<server>/<partition_name>', methods=['PUT'])
+def delete_partition(server, partition_name):
+    return 'put /delete_partition/'
+
+@app.route(f'{BASE_URI}/create_raid/<raid_name>', methods=['PUT'])
+def create_raid(raid_name):
+    return 'put /create_raid/'
+
+@app.route(f'{BASE_URI}/delete_raid/<raid_name>', methods=['PUT'])
+def delete_raid(raid_name):
+    return 'put /delete_raid/'
+
+@app.route(f'{BASE_URI}/add_raid/<raid_name>/<disk>', methods=['PUT'])
+def add_raid(raid_name, disk):
+    return 'put /add_raid/'
+
+@app.route(f'{BASE_URI}/delete_raid/<raid_name>/<disk>', methods=['PUT'])
+def delete_raid(raid_name, disk):
+    return 'put /delete_raid/'
+
+@app.route(f'{BASE_URI}/add_network_interface/<server>/<ip_adress>/<default_gateway>/<routing_table>/<interface_name>', methods=['PUT']):
+def add_network_interface(server, ip_adress, default_gateway, routing_table, interface_name):
+    return 'put /add_network_interface/'
+
+@app.route(f'{BASE_URI}/del_network_interface/<interface_name>', methods=['PUT']):
+def add_network_interface(interface_name):
+    return 'put /del_network_interface/'
+
+@app.route(f'{BASE_URI}/set_ip_rules/<interface_name>/<firewall_ip>', methods=['PUT']):
+def set_ip_rules(interface_name, firewall_ip):
+    return 'put /del_network_interface/'
+
+
+@app.route(f'{BASE_URI}/del_ip_rules/<interface_name>/', methods=['PUT']):
+def del_ip_rules(interface_name):
+    return 'put /del_network_interface/'
 
 if __name__ == '__main__':
     app.run(host=NAME, port=PORT)
