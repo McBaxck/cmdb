2fa4e6ec588150c5acbed0b42250a91b316db516
McBaxck <101018092+McBaxck@users.noreply.github.com>
2022-10-04 11:43:54 +0200

    New classes update: Route, Server, RAID, Partition, VM, DD

--- classes/server.py
+++ classes/server.py
@@ -1,14 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import List
+from dd import HardDisk
+from abc import ABC
 
 
 @dataclass
-class Server():
+class Server(ABC):
     _label: str
     _moRam: int
     _cpu: str
     _gpu: str
     _hostname: str
+    _disks: List[HardDisk]
 
     @property
     def label(self) -> str:
@@ -49,3 +52,15 @@ class Server():
     @hostname.setter
     def hostname(self, value: str) -> None:
         self._hostname = value
+
+    def __str__(self) -> str:
+        return self._cpu
+
+    def __len__(self) -> int:
+        return len(self._disks)
+
+
+if __name__ == '__main__':
+    test: Server = Server('_label', 392983, '_cpu', '_gpu', '_hostname', [])
+    print(asdict(test))
+    print(len(test))
